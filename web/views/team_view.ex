92020204bf8107a6169c66d4280903d3f60a87c3
Nuno Polonia <nunopolonia@gmail.com>
2017-06-07 12:29:19 +0100

    Change get /users to only show ids and names

--- web/views/team_view.ex
+++ web/views/team_view.ex
@@ -15,14 +15,14 @@ defmodule Api.TeamView do
     %{
       id: team.id,
       name: team.name,
-      owner: if team.owner do render_one(team.owner, UserView, "user_summary.json") end,
-      members: if team.users do render_many(team.users, UserView, "user_summary.json") end,
+      owner: if team.owner do render_one(team.owner, UserView, "user_short.json") end,
+      members: if team.users do render_many(team.users, UserView, "user_short.json") end,
       project: if team.project do render_one(team.project, ProjectView, "project.json") end,
       invites: if team.invites do render_many(team.invites, InviteView, "invite.json") end,
     }
   end
 
-  def render("team_summary.json", %{team: team}) do
+  def render("team_short.json", %{team: team}) do
     %{
       id: team.id,
       name: team.name
