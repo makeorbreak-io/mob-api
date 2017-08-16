35064904a0d803167b97e524b5df4ec9330dce09
Nuno Polonia <nunopolonia@gmail.com>
2017-08-16 16:59:02 +0100

    Add admin migration

---
+++ priv/repo/migrations/20170816155058_add_admin.exs
@@ -0,0 +1,9 @@
+defmodule Api.Repo.Migrations.AddAdmin do
+  use Ecto.Migration
+  import Ecto.Query
+
+  def change do
+    from(u in "users", where: u.email == "joao.gradim@gmail.com", update: [set: [role: "admin"]])
+    |> Api.Repo.update_all([])
+  end
+end
