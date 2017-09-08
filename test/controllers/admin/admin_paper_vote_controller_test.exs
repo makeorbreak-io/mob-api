e09e967627672a4b9389aceac7b7cd650c64e1fb
Paulo Köch <pkoch@lifeonmars.pt>
2017-09-08 22:07:02 +0100

    PaperVote endpoints

---
+++ test/controllers/admin/admin_paper_vote_controller_test.exs
@@ -0,0 +1,122 @@
+defmodule Api.Admin.PaperVoteControllerTest do
+  use Api.ConnCase
+
+  alias Api.{CompetitionActions, PaperVote}
+  alias Guardian.{Permissions}
+
+  setup %{conn: conn} do
+    admin = create_admin()
+    c = create_category()
+    pv = create_paper_vote(c, admin)
+
+    m = create_user()
+    t = create_team(m)
+
+    make_teams_eligible()
+    CompetitionActions.start_voting()
+
+    {:ok, jwt, _} =
+      Guardian.encode_and_sign(admin, :token, perms: %{admin: Permissions.max})
+
+    {:ok, %{
+      admin: admin,
+      category: c,
+      paper_vote: pv,
+      member: m,
+      team: t,
+      conn:
+        conn
+        |> put_req_header("authorization", "Bearer #{jwt}")
+        |> put_req_header("content-type", "application/json")
+    }}
+  end
+
+  test "create", %{conn: conn, category: c, admin: a} do
+    conn = post(conn, admin_paper_vote_path(conn, :create), %{
+      category_name: c.name,
+    })
+
+    new_pv = Repo.one!(from(pv in PaperVote, offset: 1))
+    assert json_response(conn, 200) == %{
+      "annulled_at" => nil,
+      "annulled_by_id" => nil,
+      "category_id" => c.id,
+      "created_by_id" => a.id,
+      "id" => new_pv.id,
+      "redeemed_at" => nil,
+      "redeeming_admin_id" => nil,
+      "redeeming_member_id" => nil,
+      "team_id" => nil,
+    }
+  end
+
+  test "show", %{conn: conn, paper_vote: pv, category: c, admin: a} do
+    conn = get(conn, admin_paper_vote_path(conn, :show, pv.id))
+
+    assert json_response(conn, 200) == %{
+      "annulled_at" => nil,
+      "annulled_by_id" => nil,
+      "category_id" => c.id,
+      "created_by_id" => a.id,
+      "id" => pv.id,
+      "redeemed_at" => nil,
+      "redeeming_admin_id" => nil,
+      "redeeming_member_id" => nil,
+      "team_id" => nil,
+    }
+  end
+
+  test "404", %{conn: conn} do
+    assert_error_sent 404, fn ->
+      get(conn, admin_paper_vote_path(conn, :show, "11111111-1111-1111-1111-111111111111"))
+    end
+  end
+
+  test "redeem", %{conn: conn, paper_vote: pv, team: t, member: m, category: c, admin: a} do
+    conn = post(conn, admin_paper_vote_path(conn, :redeem, pv.id), %{
+      team_id: t.id,
+      member_id: m.id,
+    })
+
+    pv = Repo.get!(PaperVote, pv.id)
+    assert json_response(conn, 200) == %{
+      "annulled_at" => nil,
+      "annulled_by_id" => nil,
+      "category_id" => c.id,
+      "created_by_id" => a.id,
+      "id" => pv.id,
+      "redeemed_at" => DateTime.to_iso8601(pv.redeemed_at),
+      "redeeming_admin_id" => a.id,
+      "redeeming_member_id" => m.id,
+      "team_id" => t.id,
+    }
+  end
+
+  test "redeem error", %{conn: conn, paper_vote: pv, team: t, member: m} do
+    CompetitionActions.end_voting()
+
+    conn = post(conn, admin_paper_vote_path(conn, :redeem, pv.id), %{
+      team_id: t.id,
+      member_id: m.id,
+    })
+
+    assert json_response(conn, 422) == %{"errors" => "Competition already ended"}
+  end
+
+  test "annul", %{conn: conn, paper_vote: pv, category: c, admin: a} do
+    conn = post(conn, admin_paper_vote_path(conn, :annul, pv.id))
+
+    pv = Repo.get!(PaperVote, pv.id)
+    assert json_response(conn, 200) == %{
+      "annulled_at" => DateTime.to_iso8601(pv.annulled_at),
+      "annulled_by_id" => a.id,
+      "category_id" => c.id,
+      "created_by_id" => a.id,
+      "id" => pv.id,
+      "redeemed_at" => nil,
+      "redeeming_admin_id" => nil,
+      "redeeming_member_id" => nil,
+      "team_id" => nil,
+    }
+  end
+end
---
+++ web/views/admin/admin_paper_vote_view.ex
@@ -0,0 +1,17 @@
+defmodule Api.Admin.PaperVoteView do
+  use Api.Web, :view
+
+  def render("paper_vote.json", %{paper_vote: pv}) do
+    Map.take(pv, [
+      :id,
+      :annulled_at,
+      :annulled_by_id,
+      :category_id,
+      :created_by_id,
+      :redeemed_at,
+      :redeeming_admin_id,
+      :redeeming_member_id,
+      :team_id,
+    ])
+  end
+end
