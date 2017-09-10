7a7ddf01cb3b13c9d993624ea8240c06db11ba8b
Paulo Köch <pkoch@lifeonmars.pt>
2017-09-10 12:52:53 +0100

    calculate_podium

--- test/actions/competition_actions_test.exs
+++ test/actions/competition_actions_test.exs
@@ -33,3 +33,79 @@ defmodule Api.CompetitionActionsTest do
     {:error, :already_ended} = CompetitionActions.end_voting()
   end
 end
+
+defmodule Api.CompetitionActionsTest.CalculatePodium do
+  use Api.ModelCase
+
+  alias Api.{CompetitionActions}
+  alias Ecto.{Changeset}
+
+  setup do
+    cat = create_category()
+
+    u1 = create_user()
+    t1 = create_team(u1)
+
+    u2 = create_user()
+    t2 = create_team(u2)
+
+    u3 = create_user()
+    t3 = create_team(u3)
+
+    u4 = create_user()
+    t4 = create_team(u4)
+
+    check_in_everyone()
+    make_teams_eligible()
+    CompetitionActions.start_voting()
+
+    {
+      :ok,
+      %{
+        u1: u1,
+        u2: u2,
+        u3: u3,
+        t1: t1,
+        t2: t2,
+        t3: t3,
+        t4: t4,
+        category: cat,
+      },
+    }
+  end
+
+  test "calculate_podium", %{category: cat, t1: t1, t2: t2, t3: t3, t4: t4} do
+    create_vote(create_user(), cat.name, [t1.id, t2.id])
+    create_vote(create_user(), cat.name, [t1.id])
+    create_vote(create_user(), cat.name, [t1.id])
+    create_vote(create_user(), cat.name, [t2.id])
+    create_vote(create_user(), cat.name, [t2.id])
+    create_vote(create_user(), cat.name, [t3.id])
+    create_vote(create_user(), cat.name, [t3.id])
+    create_vote(create_user(), cat.name, [t4.id])
+
+    assert CompetitionActions.calculate_podium(cat) == [
+      t1.id,
+      t2.id,
+      t3.id,
+    ]
+  end
+
+  test "calculate_podium tie", %{category: cat, t1: t1, t2: t2, t4: t4} do
+    create_vote(create_user(), cat.name, [t1.id])
+    create_vote(create_user(), cat.name, [t1.id])
+    create_vote(create_user(), cat.name, [t2.id])
+    create_vote(create_user(), cat.name, [t2.id])
+    create_vote(create_user(), cat.name, [t4.id])
+
+    Changeset.change(t4, tie_breaker: 0) |> Repo.update!
+    Changeset.change(t2, tie_breaker: 10) |> Repo.update!
+    Changeset.change(t1, tie_breaker: 20) |> Repo.update!
+
+    assert CompetitionActions.calculate_podium(cat) == [
+      t2.id,
+      t1.id,
+      t4.id,
+    ]
+  end
+end
--- web/actions/competition_actions.ex
+++ web/actions/competition_actions.ex
@@ -1,7 +1,8 @@
 defmodule Api.CompetitionActions do
   use Api.Web, :action
 
-  alias Api.{Competition, TeamActions}
+  alias Api.{Competition, TeamActions, Vote, PaperVote, Team}
+  alias Ecto.{Changeset}
 
   defp _get do
     Repo.one(from(c in Competition)) || %Competition{}
@@ -50,5 +51,56 @@ defmodule Api.CompetitionActions do
 
   def voting_ended_at do
     _get().voting_ended_at
+  end
+
+  def ballots(category) do
+    votes =
+      Repo.all(from(
+        v in Vote,
+        where: v.category_id == ^(category.id)
+      ))
+      |> Enum.map(&({&1.voter_identity, &1.ballot}))
+
+    paper_votes =
+      Repo.all(from(
+        pv in PaperVote.countable(),
+        where: pv.category_id == ^(category.id)
+      ))
+      |> Enum.map(&({&1.id, [&1.team_id]}))
+
+    (paper_votes ++ votes)
+  end
+
+  def calculate_podium(category) do
+    valid_team_ids =
+      Repo.all(Team.votable(), select: :id)
+      |> Enum.map(&Map.get(&1, :id))
+
+    votes =
+      ballots(category)
+      |> Enum.map(fn {_id, ballot} ->
+        Enum.filter(ballot, &Enum.member?(valid_team_ids, &1))
+      end)
+      |> Enum.reject(&Enum.empty?/1)
+
+    votes
+    |> Enum.flat_map(&Markus.ballot_to_pairs(&1, valid_team_ids))
+    |> Markus.pairs_to_preferences(valid_team_ids)
+    |> Markus.normalize_margins(valid_team_ids)
+    |> Markus.widen_paths(valid_team_ids)
+    |> Markus.rank_candidates(valid_team_ids)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.flat_map(fn level ->
+      Enum.map(
+        level,
+        fn team_id ->
+          t = Repo.get!(Team, team_id)
+          {t.tie_breaker, t.id}
+        end
+      )
+      |> Enum.sort
+      |> Enum.map(&elem(&1, 1))
+    end)
+    |> Enum.take(3)
   end
 end
