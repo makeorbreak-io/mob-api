eaa2ca06c6fc0106b957f40114615cf5cbf00f20
Nuno Polonia <nunopolonia@gmail.com>
2017-09-29 03:32:37 +0100

    Remove old scripts from migrations

--- priv/repo/migrations/20170620151128_remove_accepted_and_add_email_to_invites.exs
+++ priv/repo/migrations/20170620151128_remove_accepted_and_add_email_to_invites.exs
@@ -6,7 +6,5 @@ defmodule Api.Repo.Migrations.RemoveAcceptedAndAddEmailToInvites do
       add :email, :string
       remove :accepted
     end
-
-    # drop unique_index(:invites, [:team_id, :invitee_id])
   end
 end
